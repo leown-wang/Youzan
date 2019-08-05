51bde660e11c953fdb7a90607b6709ec097df173
Leown <leown_wang@163.com>
2019-08-05 16:23:01 +0800

    completed cart page common function 1. completed select funcion. 2. completed allPrice calculation

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -11,7 +11,27 @@ new Vue({
   el:'.container',
   data:{
     lists:null,
-
+    allChecked: true,
+    total:0
+  },
+  computed: {
+    selectLists() {
+      if(this.lists&&this.lists.length){
+        let arr = []
+        let total = 0
+        this.lists.forEach(shop => {
+          shop.goodsList.forEach(good => {
+            if(good.checked) {
+              arr.push(good)
+              total += good.price * good.number
+            }
+          })
+        })
+        this.total = total
+        return arr
+      }
+      return []
+    },
   },
   created() {
     this.getLists()
@@ -19,7 +39,45 @@ new Vue({
   methods:{
     getLists(){
       axios.get(url.cartLists).then(res=>{
-        this.lists = res.data.cartList
+        let lists = res.data.cartList
+        lists.forEach(shop =>{
+          shop.checked = true
+          shop.goodsList.forEach(good=>{
+            good.checked = true
+          })
+        })
+        this.lists = lists
+      })
+    },
+    selectGood(good,shop){
+      good.checked = !good.checked
+      if(good.checked ==false) this.allChecked == false
+      shop.checked = shop.goodsList.every(good =>{
+        return good.checked
+      })
+      this.allChecked = this.lists.every(shop=>{
+        return shop.goodsList.every(good =>{
+          return good.checked
+        })
+      })
+    },
+    selectShop(shop){
+      shop.checked = !shop.checked
+      if (shop.checked == false) this.allChecked == false
+      shop.goodsList.forEach(good => {
+        good.checked = shop.checked
+      })
+      this.allChecked = this.lists.every(shop=>{
+        return shop.checked
+      })
+    },
+    selectAll(){
+      this.allChecked = !this.allChecked
+      this.lists.forEach(shop=>{
+        shop.checked = this.allChecked
+        shop.goodsList.forEach( good=>{
+          good.checked = this.allChecked
+        })
       })
     }
   },
